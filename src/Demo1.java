0e48f6246b44d8058330f4662c28a05d20d305fc
Juno1994 <43963934+Juno1994@users.noreply.github.com>
2018-10-10 12:33:40 +0800

    Update Demo1.java

--- src/Demo1.java
+++ src/Demo1.java
@@ -2,6 +2,7 @@ public class Demo1 extends Parent {
     int i = 1;
     String a ="xixi";
     int j = 2;
+    String b="bili";
 
     public void test(){
         System.out.println("hello gitHub");
